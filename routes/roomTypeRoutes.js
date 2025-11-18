35e0b2822411b00938af02c175ec502e2fccc653
cogsonf <sondoan741@gmail.com>
2025-11-18 10:03:41 +0700

    feat/roomtype(mana)

--- routes/roomTypeRoutes.js
+++ routes/roomTypeRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const roomController = require('../controllers/roomController');
-const { protect, isHotelManager } = require('../middlewares/auth.middleware');
+const { protect, isHotelManagerOrAdmin } = require('../middlewares/auth.middleware');
 
-// Room Type routes
-router.get('/', protect, isHotelManager, roomController.getAllRoomTypes);
-router.get('/:id', protect, isHotelManager, roomController.getRoomTypeById);
-router.post('/', protect, isHotelManager, roomController.createRoomType);
-router.put('/:id', protect, isHotelManager, roomController.updateRoomType);
-router.delete('/:id', protect, isHotelManager, roomController.deleteRoomType);
+// Room Type routes - global resources, managers and admins can access without hotelId
+router.get('/', protect, isHotelManagerOrAdmin, roomController.getAllRoomTypes);
+router.get('/:id', protect, isHotelManagerOrAdmin, roomController.getRoomTypeById);
+router.post('/', protect, isHotelManagerOrAdmin, roomController.createRoomType);
+router.put('/:id', protect, isHotelManagerOrAdmin, roomController.updateRoomType);
+router.delete('/:id', protect, isHotelManagerOrAdmin, roomController.deleteRoomType);
 
 module.exports = router;
